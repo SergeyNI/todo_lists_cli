a1f58b051ff447e61024f8b4e0e247a3e76a402c
Сергій Нумержицький <rist1cnum@gmail.com>
2022-11-29 22:05:59 +0200

    added moving up, down along list

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,5 +7,15 @@ import Board.*
   val listInProgress = new TaskList(1,"IN PROGRESS")
   val taskcreate = new Task(1,"Create program TODO","create on scala program TODO BOARD",listTODO)
   val taskTest = new Task(2,"Test program ","TEST program",listTODO)
-  println(listTODO.show)
+  val taskDeploy = new Task(3,"DEPLOY ","DEPLOY program",listTODO)
+  val taskView = new Task(4,"VIEW ","VIEW program",listTODO)
+  listTODO.show
+  // listTODO.up(taskDeploy)
+  // listTODO.up(taskDeploy)
+  // listTODO.up(taskDeploy)
+  listTODO.down(taskcreate)
+  listTODO.down(taskcreate)
+  listTODO.down(taskcreate)
+  //listTODO.down(taskcreate)
+  listTODO.show
 
--- src/main/scala/TaskList.scala
+++ src/main/scala/TaskList.scala
@@ -7,8 +7,40 @@ import scala.collection.mutable.ListBuffer
 class TaskList(id:Int, name:String, private var tasks: ListBuffer[Task]=ListBuffer.empty[Task]) extends NamedObject {
 
   def show:Unit= 
-    println(s"$this"+'\n')
-    tasks.foreach(task=>println(task))
+    println('\n'+Console.MAGENTA+s"$this"+'\n')
+    tasks.foreach(task=>println(Console.CYAN+task))
   def add(task:Task) = tasks.append(task)
   override def toString(): String = s"($id) $name"
+  def up(task:Task):Unit =
+    val index = tasks.indexOf(task)
+    val prevNumber = index match
+      case 0 => index
+      case x => x-1
+
+    val topTasks = tasks.take(prevNumber)
+
+    topTasks.append(task)
+    if index >0 then 
+      val prevTask = tasks(index-1)
+      topTasks.append(prevTask)
+
+    val bottomTasks = tasks.drop(index+1)
+    tasks = topTasks concat bottomTasks
+  
+  def down(task:Task):Unit =
+    val index = tasks.indexOf(task)
+    val numberElementOfListBeforeCurrentElement =  index
+    val topTasks = tasks.take(numberElementOfListBeforeCurrentElement)
+    // val nextIndex = index match
+    //   case tasks.length => index
+    //   case x => x+1
+    if index < (tasks.length-1) then
+      val nextTask = tasks(index+1)
+      topTasks.append(nextTask)
+    topTasks.append(task)
+
+    if index <= (tasks.length-2) then
+      val bottomTasks = tasks.drop(index+2) //after next element
+      tasks = topTasks concat bottomTasks
+
 }
