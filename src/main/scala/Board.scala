c533ce04e10787c1019ac3bc779626549b0f319a
Сергій Нумержицький <rist1cnum@gmail.com>
2022-12-04 09:15:28 +0200

    refactorng Board, Board viewer advanced Commander

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -2,27 +2,20 @@ package Board
 import TaskList.TaskList
 import scala.collection.mutable.ListBuffer
 
-case class Board(name:String,var taskLists:Option[ListBuffer[TaskList]], var currentTaskList:Option[TaskList]){
-
-  def setCurrentList(list:TaskList) = currentTaskList = Some(list)
+case class Board(name:String,var taskLists:ListBuffer[TaskList] = ListBuffer.empty[TaskList], var currentTaskList:Option[TaskList] = None){
+  override def toString(): String = name
+  def setCurrentList(list:TaskList) = 
+    println(s"On board $this seted current tasklist $list")
+    currentTaskList = Some(list)
+  
   def getCurrentTaskList():Option[TaskList] = currentTaskList
-  def getLists() = taskLists match
-    case None => new ListBuffer[TaskList]
-    case Some(lists) => lists
-  def add(list:TaskList) =
-    val lists = taskLists match
-      case None => 
-        val l = new ListBuffer[TaskList]
-        l.append(list)
-      case Some(lists) => lists.append(list)
-    taskLists = Some(lists)
+  
+  def getLists() = taskLists
+  def add(list:TaskList) = taskLists.append(list)
+  def list(index:Int):Option[TaskList] = if index >=0 && index < taskLists.length then Some(taskLists(index)) else None
 
   def remove(list:TaskList) = {
-    val lists = taskLists match {
-      case Some(slists) => slists
-      case None => new ListBuffer[TaskList]
-    }
-    Some(lists.filterNot(currlist => currlist == list))
+    taskLists = taskLists.filterNot(currlist => currlist == list)
   }
 
 
