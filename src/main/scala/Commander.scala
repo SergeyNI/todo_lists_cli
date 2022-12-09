0107770dacf6162517cd15f4832a12f1c1f1cd5d
Serhii <rist1cnum@gmail.com>
2022-12-09 17:30:51 +0200

    .

--- src/main/scala/Commander.scala
+++ src/main/scala/Commander.scala
@@ -6,18 +6,26 @@ import Board.Board
 import BoardViewer.BoardViewer
 
 import scala.compiletime.ops.any
+// trait Command
+// case class sl(str:String) extends Сommand
+// case class al(str:String) extends Сommand
+// case class quit(str:String) extends Сommand
+
+
 class Commander(board: Board):
   def showCommands: Unit =
-    val str1 = s"sl -listID # -select current list"+'\n'+
+    val str1 = s"Aviable commands:"+'\n'+
+                "sl -listID # -select current list"+'\n'+
                 "al nameOfList # add new list"+'\n'+
                 "quit # quit of program"+'\n'
+                "help # print this info"+'\n'
                 
-    println(str1)
+    println(Console.BLUE+str1)
 
   private def sl: PartialFunction[String, Boolean] =
     new PartialFunction[String, Boolean] {
       // States that this partial function will take on the task
-      def isDefinedAt(str: String) = str.startsWith("sl")
+      def isDefinedAt(str: String) = str.startsWith("sl ") && str.length() > 3
       // What we do if this partial function matches
       def apply(str: String) =
         def toInt(s: String): Option[Int] = {
@@ -79,6 +87,7 @@ class Commander(board: Board):
       // What we do if this partial function matches
       def apply(str: String) =
         println(s"command $str not found")
+        showCommands
         true
     }
 
@@ -88,6 +97,30 @@ class Commander(board: Board):
   private def executeCommand(str:String):Boolean = 
     val command = sl orElse help orElse al orElse quit orElse notfound
     command(str)
+    // def toInt(s: String): Option[Int] = 
+    //   try 
+    //     Some(s.toInt)
+    //   catch 
+    //     case e: Exception => None
+    // str match
+    //   case x if x.startsWith("sl ") && x.length() > 3  =>
+        
+    //     val strArg = str.substring(3)
+    //     val argOption = toInt(strArg)
+    //     val arg = argOption match
+    //       case None =>
+    //         println(s"can't convert index  of list $strArg to Int")
+    //         -1
+    //       case Some(x) => x
+        
+    //     val sometaskList: Option[TaskList] = board.list(arg)
+    //     sometaskList match
+    //       case Some(taskList) => board.setCurrentList(taskList)
+    //       case None           => println(s"can't find task list by index $arg")
+        
+    //     BoardViewer(board).showBoard
+    //     true 
+    
 
   def inputCommand =
     println(Console.WHITE+"Input command:")
